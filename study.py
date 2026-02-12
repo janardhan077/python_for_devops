5a4710efd8184204e815e8ff7f1659c222f3f332
JANARDHAN <jana020711@gmail.com>
2026-02-12 23:31:46 +0530

    Create a number addition program in study.py
    Add a simple program to add two numbers and check if the result is 8.

---
+++ study.py
@@ -0,0 +1,11 @@
+ #hi how are u 
+ 
+num1 = float(input ("enter the num one :"))
+num2 = float(input("enter the num two "))
+
+ans=num1 + num2
+    
+if (ans==8):
+        print("u have got the ans correctly my logical is running coreectly ")
+else :
+          print(type(num1))  
