9bc003554540f315452810f9181d7ed10faa5bb9
JANARDHAN <jana020711@gmail.com>
2026-02-12 23:37:26 +0530

    Implement multiplication table with input loop
    Fix input prompts and correct loop structure.

---
+++ for_loops.py
@@ -0,0 +1,7 @@
+ get = input("enetr q quit the lopp ")
+    while get != "q" :
+        num =int(input(" enter the number of the table u want "))
+for i in range (1,11):
+    ans=num * i 
+    print( f"{num} x {i} = {ans}")
+get = input("enetr q quit the lopp ")
