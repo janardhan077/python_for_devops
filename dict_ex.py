cb240d5c24a678ac078dacc948de71b29740d2f3
JANARDHAN <jana020711@gmail.com>
2026-02-14 22:05:38 +0530

    Add dictionary example with name and age
    Initialize a dictionary with name and age, and print its contents.

---
+++ dict_ex.py
@@ -0,0 +1,9 @@
+info = {
+    "name" :"jana",
+    "age" : 20 ,
+    
+}
+print ("my name is " ,info["name"])
+print (" my age is ",info.get ("agg"))
+for key,value in info.items():
+    print (key,value)
