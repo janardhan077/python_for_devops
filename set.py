f98b86ff967b7c64a63455155094fc9eeee2602e
JANARDHAN <jana020711@gmail.com>
2026-02-15 17:09:02 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,7 @@
+info = {} #empty dir
+days =set () #empty set 
+days= {"saturday","sunday","sunday","saturday"}
+print (days)
+num = [2,4,4,4,2,2,4,6,8,7,473,3,45,457,2457]
+num = list(set(num))
+print (num)
